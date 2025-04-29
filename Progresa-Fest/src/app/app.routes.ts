b964f0f65238164917f7ceb1bca59c2c47c67fc4
stanis <stanislav.kalynovskyi@cieep.com>
2025-04-29 13:29:32 +0200

    vinculo de login y registro

--- Progresa-Fest/src/app/app.routes.ts
+++ Progresa-Fest/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
 import { EntradasComponent } from './componentes/entradas/entradas.component';
 import { InfoComponent } from './componentes/info/info.component';
 import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/inicio', pathMatch: 'full'},
@@ -12,4 +14,6 @@ export const routes: Routes = [
   { path: 'entradas', component: EntradasComponent },
   { path: 'info', component: InfoComponent },
   { path: 'contacto', component: ContactoComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'login', component: LoginComponent },
 ];
